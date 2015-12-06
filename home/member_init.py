9b6536f3cfa41b0cd716acdd3eb9a0332471ea40
Rick7Xu <rick_xu7@hotmail.com>
2015-12-06 16:10:53 +0800

    Added 2 members for test

--- home/member_init.py
+++ home/member_init.py
@@ -13,12 +13,12 @@ django.setup()
 from home.models import Member
 Member.objects.all().delete()
 Member.objects.bulk_create([
-    Member(name='', name_ch='', nick_name='', number=1,
-           pos1='', pos2='', pos3='',
+    Member(name='chen', name_ch='陈华', nick_name='陈老师', number=1,
+           pos1='守门员', pos2='', pos3='',
            level=0, balance=0, physique=0, bug=0),
-    Member(name='', name_ch='', nick_name='', number=1,
-           pos1='', pos2='', pos3='',
-           level=0, balance=0, physique=0, bug=0),
+    Member(name='ben', name_ch='俞斌辉', nick_name='本本', number=2,
+           pos1='防守型中场', pos2='中后卫', pos3='攻击型中场',
+           level=1, balance=0, physique=1, bug=0),
 ])
 
 print('Done')
