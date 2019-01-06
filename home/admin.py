9930c67bc6c9206db867e845cc8f09a99d535cf1
Zhang Jie <jzhang14@paypal.com>
2019-01-06 18:55:34 +0800

    update

--- home/admin.py
+++ home/admin.py
@@ -5,7 +5,7 @@ from .models import Member
 @admin.register(Member)
 class MemberAdmin(admin.ModelAdmin):
     # empty_value_display = '-empty-'
-    list_display = ('name', 'name_chn', 'nick_name', 'number', 'pos1', 'pos2', 'pos3', 'top_star', 'all_pos', 'stamina', 'athletic', 'grade')
+    list_display = ('name', 'name_chn', 'nick_name', 'number', 'top_star', 'all_pos', 'stamina', 'athletic', 'grade')
 
 
 
